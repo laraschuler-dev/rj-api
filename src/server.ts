cd920671703435b07e1f61317665d8863a597e0f
candidocimol <candido.cimol@gmail.com>
2025-02-03 17:46:26 -0300

    Connection to database

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
+import { PrismaClient } from '@prisma/client';
 import routes from './routes'; // Importa o arquivo principal de rotas
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient(); // Instância do Prisma Client
 
 app.use(cors());
 app.use(helmet());
@@ -16,6 +18,7 @@ app.use(express.json());
 
 // Usa as rotas definidas no arquivo src/routes/index.ts
 app.use('/api', routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
